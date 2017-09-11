07cd04366a9e4bae18dad24877d2f3ca6c061370
luzbinbautista@hotmail.com <luzbinbautista@hotmail.com>
2017-09-10 22:28:11 -0500

    //
    //

--- Plantillas/Plantilla_ad9833/Plantilla_ad9833.ino
+++ Plantillas/Plantilla_ad9833/Plantilla_ad9833.ino
@@ -1,7 +1,8 @@
 
 #include <SPI.h>
 
-const int slaveSelectPin = 10;       // Comando para escribir en el AD5933 
+const int slaveSelectPin_DDS1 = 10;       // Comando para escribir en el AD5933 
+const int slaveSelectPin_DDS2 = 9;       // Comando para escribir en el AD5933 
 
   //* CS - to digital pin 10  (SS pin)
   //* SDI - to digital pin 11 (MOSI pin)
@@ -11,14 +12,17 @@ const int slaveSelectPin = 10;       // Comando para escribir en el AD5933
 
 #define this_pin 5
 void setup() {
-  pinMode(this_pin,OUTPUT);
-  pinMode(slaveSelectPin, OUTPUT);
+  pinMode(slaveSelectPin_DDS1,OUTPUT);
+  pinMode(slaveSelectPin_DDS2, OUTPUT);
   // initialize SPI:
   SPI.begin();
   delay(10);
   for (int hola = 1; hola < 10; hola++) {
-    SET_AD9833();
+    SET_DDS_1();
     delay(20);
+    SET_DDS_2();
+    delay(20);
+    
     }
   Serial.begin(9600);
 }
@@ -27,26 +31,43 @@ void loop() {
   
 }
 
-void SET_AD9833() {
+
+
+void SET_DDS_1() {
+
   // take the SS pin low to select the chip:
   SPI.beginTransaction(SPISettings(SPI_CLOCK_SPEED, MSBFIRST, SPI_MODE2));   
   
-  digitalWrite(slaveSelectPin, LOW);
+  digitalWrite(slaveSelectPin_DDS1, LOW);
   //  send in the address and value via SPI:
   
-  SPI.transfer16(0x2128);//2128
-  SPI.transfer16(0x69F1); //01 + dato de frecuencia que son 14 bits
-  //SPI.transfer16(0x50C7); //01 + el resto del dato de frecuencia
-  SPI.transfer16(0x4000); //Fase, No mover
-  SPI.transfer16(0xC000); //Fase, No mover 
-  SPI.transfer16(0x2028);
+  SPI.transfer16(0x2128); //Trama de configuración y activación del reset
+  SPI.transfer16(0x4000); //01 + dato de frecuencia que son 14 bits los menos significativos
+  SPI.transfer16(0x4041); //01 + dato de frecuencia que son 14 bits los mas significativos
+  SPI.transfer16(0xC000); //Fase, No mover se va a dejar en cero, en este caso no importa =)
+  SPI.transfer16(0x2028); //Trama de configuración y desactivación del reset
   
   // take the SS pin high to de-select the chip:
-  digitalWrite(slaveSelectPin, HIGH);
+  digitalWrite(slaveSelectPin_DDS1, HIGH);
   SPI.endTransaction();
 }
 
-//0010 0000 0010 0000
-//0010 0000 0000 0010
+void SET_DDS_2() {
 
-//00010 0001 0010 0010
+  // take the SS pin low to select the chip:
+  SPI.beginTransaction(SPISettings(SPI_CLOCK_SPEED, MSBFIRST, SPI_MODE2));   
+  
+  digitalWrite(slaveSelectPin_DDS2, LOW);
+  //  send in the address and value via SPI:
+  
+  SPI.transfer16(0x2128); //Trama de configuración y activación del reset
+  SPI.transfer16(0x67C5); //01 + dato de frecuencia que son 14 bits los menos significativos
+  SPI.transfer16(0x4002); //01 + dato de frecuencia que son 14 bits los mas significativos
+  SPI.transfer16(0xC000); //Fase, No mover se va a dejar en cero, en este caso no importa =)
+  SPI.transfer16(0x2028); //Trama de configuración y desactivación del reset
+  
+  // take the SS pin high to de-select the chip:
+  digitalWrite(slaveSelectPin_DDS2, HIGH);
+  SPI.endTransaction();
+}
+
